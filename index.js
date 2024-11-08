df9ab26310777d02a8844e6db8cbe745861feff4
Tyler <tbos92@gmail.com>
2024-11-07 19:48:35 -0500

    try again

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const pg = require("pg");
 const express = require("express");
+const app = express();
 const client = new pg.Client(
   process.env.DATABASE_URL ||
     "postgres://bosma:3892@localhost/the_acme_notes_db"
 );
+
+const init = async () => {
+  const port = process.env.PORT || 3000;
+  application.listen(port, () => console.log(`listening on port ${port}`));
+};
+init();
